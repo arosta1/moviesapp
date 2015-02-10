b5f864f64dc522d5aa3fe12b51d798e4be8cc787
Aaron <c12444752@mydit.ie>
2015-02-10 15:40:20 +0000

    Login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Moviesapp::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -34,4 +34,16 @@ Moviesapp::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  #SMTP Delivery
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+	:address => 'smtp.gmail.com',
+	:port => 587,
+	:domain => 'www.example.com',
+	:authentication => :plain,
+	:user_name => 'zimmer.skimmer@gmail.com',
+	:password => 'zApple123'
+  }
 end
